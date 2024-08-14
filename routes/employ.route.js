e67d6dd2927d306012b4e8fe2226b4e9f1de8845
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-08-14 14:00:50 +0700

    create get emp by id route

--- routes/employ.route.js
+++ routes/employ.route.js
@@ -2,10 +2,16 @@ const express = require("express");
 const {
   createEmployment,
   getAllEmployment,
+  getEmploymentById,
 } = require("../controller/employ.controller");
 const cloudinaryFileUploader = require("../middlewares/FileUploader");
 const router = express.Router();
 
-router.post("/", cloudinaryFileUploader.single('profileImage') , createEmployment);
+router.post(
+  "/",
+  cloudinaryFileUploader.single("profileImage"),
+  createEmployment
+);
 router.get("/", getAllEmployment);
+router.get("/:id", getEmploymentById);
 module.exports = router;

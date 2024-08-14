0e3535431375451cc782314f464c9056e88aefdc
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-08-14 14:00:50 +0700

    create pagination and search fun

--- controller/employ.controller.js
+++ controller/employ.controller.js
@@ -1,7 +1,7 @@
 const Employment = require("../models/employment.model");
 
 const createEmployment = async (req, res) => {
-  const { name, email, phone, department, salary, profileImage } = req.body;
+  const { name, email, phone, department, salary } = req.body;
   req.body.profileImage = req.file ? req.file?.path : null;
   if (!name)
     return res
@@ -44,6 +44,36 @@ const createEmployment = async (req, res) => {
   }
 };
 
-const getAllEmployment = async (req, res) => {};
+const getAllEmployment = async (req, res) => {
+  try {
+    const allEmployment = await Employment.find();
+    res.status(200).json({
+      message: "All employment fetched successfully",
+      success: true,
+      data: allEmployment,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
 
-module.exports = { createEmployment, getAllEmployment };
+const getEmploymentById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const emp = await Employment.findById(id);
+    return res.status(200).json({
+      message: "Employment fetched successfully",
+      data: emp,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
+};
+
+module.exports = { createEmployment, getAllEmployment, getEmploymentById };
